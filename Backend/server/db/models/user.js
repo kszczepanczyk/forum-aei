6f436a9fb9d95d167ec536be9b5ba03aa6c84f96
AgataPrzykaza <72311690+AgataPrzykaza@users.noreply.github.com>
2023-01-03 10:37:39 +0100

    poprawione

--- Backend/server/db/models/user.js
+++ Backend/server/db/models/user.js
@@ -1,8 +1,50 @@
+// const mongoose = require('mongoose');
+
+// const UserSchema = new mongoose.Schema({
+//   idUser: {
+//     type: Number,
+//     unique: true
+//   },
+//   username: {
+//     type: String,
+//     required: true,
+//     min: 4
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true
+//   },
+//   password: {
+//     type: String,
+//     required: true
+//   },
+//   rank: {
+//     type: String,
+//     required: false,
+//     default: null
+//   },
+//   date_joined: {
+//     type: Date,
+//     default: Date.now
+//   }
+// });
+
+
+// const UserModel = mongoose.model('UserModel', UserSchema);
+
+// module.exports = UserModel;
+
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   idUser: {
     type: Number,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
     unique: true
   },
   username: {
@@ -10,27 +52,23 @@ const UserSchema = new mongoose.Schema({
     required: true,
     min: 4
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
   rank: {
     type: String,
-    required: false,
+    required: true,
     default: null
   },
   date_joined: {
     type: Date,
     default: Date.now
+  },
+  password: {
+    type: String,
+    required: true
   }
 });
 
 
-const UserModel = mongoose.model('UserModel', UserSchema);
 
-module.exports = UserModel;
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
