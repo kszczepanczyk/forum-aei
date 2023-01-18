42c59ee379e3ce8a3cae0d8fbf0e20e5e0e3ac70
Katarzyna Szczepańczyk <k.szczepanczyk83@gmail.com>
2023-01-18 23:51:54 +0100

    Added clickable post

--- src/app/post/post-details/post-details.component.ts
+++ src/app/post/post-details/post-details.component.ts
@@ -1,30 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Post } from '../../models/post.model';
 import { PostService } from '../post.service';
-
 
 @Component({
   selector: 'app-post-details',
   templateUrl: './post-details.component.html',
-  styleUrls: ['./post-details.component.scss']
+  styleUrls: ['./post-details.component.scss'],
 })
 export class PostDetailsComponent implements OnInit {
-
   post: Post;
   id: number;
 
-  constructor(private postService: PostService,
-              private route: ActivatedRoute) { }
+  constructor(
+    private postService: PostService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.route.params
-    .subscribe(
-      (params: Params) => {
-        this.id = +params['id'];
-        //this.post = this.postService.getPostById(this.id);
-      }
-    )
+    this.post = {} as Post;
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.postService.getPostById(this.id).subscribe((post) => {
+          if (Object.keys(post).length === 0) {
+            this.router.navigate(['/not-found']);
+          }
+          this.post = post;
+      });
+    });
   }
-
+  
 }
